0208c9b98433d1cc60f27b98581f8dc82831bf51
Harold Elías <129306594+HamesEl@users.noreply.github.com>
2024-11-06 18:56:14 -0500

    feat(update): Update main VetConnectApplication

--- src/main/java/com/vetconnect/vetconnect/VetconnectApplication.java
+++ src/main/java/com/vetconnect/vetconnect/VetconnectApplication.java
@@ -12,4 +12,17 @@ public class VetconnectApplication {
     public static void main(String[] args) {
         SpringApplication.run(VetconnectApplication.class, args);
     }
+    @Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
