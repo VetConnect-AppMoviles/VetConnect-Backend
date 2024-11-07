a41ce44c930fb47b135a4fc040333289ac200367
Rodrigo <129321935+RodrLH@users.noreply.github.com>
2024-11-06 19:05:07 -0500

    Update VetconnectApplication.java

--- src/main/java/com/vetconnect/vetconnect/VetconnectApplication.java
+++ src/main/java/com/vetconnect/vetconnect/VetconnectApplication.java
@@ -2,7 +2,11 @@ package com.vetconnect.vetconnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -13,16 +17,16 @@ public class VetconnectApplication {
         SpringApplication.run(VetconnectApplication.class, args);
     }
     @Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
-	}
+    public static class Myconfiguration{
+        @Bean
+        public WebMvcConfigurer corsConfigurer(){
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                }
+            };
+        }
+    }
 }
